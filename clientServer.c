62dbf4cc1503e067a305c5ea986876f7d611bb8e
admin <admin@TretyakovaJ.local>
2021-07-16 19:30:07 +0300

    beauty

--- clientServer.c
+++ clientServer.c
@@ -1,12 +1,9 @@
 #include "clientServer.h"
 
-
 int main(int argc, char **argv) {
 
-    if(argc != 2) {
-        perror("Error: see README");
-        exit(1);
-    }
+    if(argc != 2) 
+        err_n_die("Error: see README");
     
     if(!strcmp(SERVER, argv[1])) {
         int status = Server();
@@ -16,24 +13,19 @@ int main(int argc, char **argv) {
         int status = Client();
         exit(status);
     }
-    else {
-        perror("Error: see README");
-        exit(1);
-    }
+    else 
+        err_n_die("Error: see README");
 }
 
 int Server() {
 
     int listenfd, connfd;
-    struct sockaddr_in serv_addr;
-
     char sendBuff[1025];
     char resvBuff[1024];
+    struct sockaddr_in serv_addr;
 
-    if( (listenfd = socket(AF_INET, SOCK_STREAM, 0)) < 0){
-        perror("\n Error : Could not create socket \n");
-        return 1;
-    }
+    if( (listenfd = socket(AF_INET, SOCK_STREAM, 0)) < 0)
+        err_n_die("Error : Could not create socket");
 
     memset(&serv_addr, '0', sizeof(serv_addr));
     memset(sendBuff, '0', sizeof(sendBuff));
@@ -43,53 +35,33 @@ int Server() {
     serv_addr.sin_addr.s_addr = htonl(INADDR_ANY);
     serv_addr.sin_port = htons(MYPORT);
     
-    if ( (bind(listenfd, (struct sockaddr*)&serv_addr, sizeof(serv_addr))) != 0) {
-        perror("\n Error : Could not bind \n");
-        return 1;
-    }
+    if ( (bind(listenfd, (SA*)&serv_addr, sizeof(serv_addr))) != 0)
+        err_n_die("Error : Could not bind");
 
-    if ( (listen(listenfd, 10)) != 0) {
-        perror("\n Error : Could not listen \n");
-        return 1;
-    }
-    
+    if ( (listen(listenfd, 10)) != 0)
+        err_n_die("Error : Could not listen");
+
     while(1) {
-        if ( (connfd = accept(listenfd, (struct sockadd *)NULL, NULL)) == -1) {
-            perror("\n Error : Could not accept \n");
-            return 1;
-        }
+        if ( (connfd = accept(listenfd, NULL, NULL)) == -1)
+            err_n_die("Error : Could not accept");
 
-        resvArgs_t s_resv;
-        s_resv.connfd = connfd;
-        s_resv.resvBuff = resvBuff;
+        write(1, ">", 1);
+        //two struct for read/write buffer
+        funcArgc_t s_resv, s_send;
 
-        sendArgs_t s_send;
-        s_send.connfd = connfd;
-        s_send.sendBuff = sendBuff;
+        s_resv.fd = connfd;
+        s_resv.buff = resvBuff;
+        s_send.fd = connfd;
+        s_send.buff = sendBuff;
         
-
+        //create two threads for Read() and Write() func
         pthread_t read_p, write_p;
-        if(pthread_create(&read_p, NULL, &Read, (void*) &s_resv) != 0) {
-            perror("\n pthread read_p create fail\n");
-            return 1;
-        }
-        if(pthread_create(&write_p, NULL, &Write, (void*) &s_send) != 0) {
-            perror("\n pthread write_p create fail\n");
-            return 1;
-        }
-        if(pthread_join(read_p, NULL) != 0) {
-            perror("\n pthread read_p join fail\n");
-            return 1;
-        }
-        if(pthread_join(write_p, NULL) != 0) {
-            perror("\n pthread read_p join fail\n");
-            return 1;
-        }
 
+        create_thread(&read_p, &Read, &s_resv);
+        create_thread(&write_p, &Write, &s_send);
 
-
-        //write(connfd, stdinBuff, strlen(stdinBuff)); 
-        //write(connfd, sendBuff, strlen(sendBuff));
+        join_thread(&read_p);
+        join_thread(&write_p);
 
         close(connfd);
     }
@@ -108,67 +80,63 @@ int Client() {
     memset(sendBuff, '0', sizeof(sendBuff));
     memset(&serv_addr, '0', sizeof(serv_addr));
 
-    if( (sockfd = socket(AF_INET, SOCK_STREAM, 0)) < 0) {
-        perror("\n Error : Could not create socket \n");
-        return 1;
-    }
+    if( (sockfd = socket(AF_INET, SOCK_STREAM, 0)) < 0)
+        err_n_die("Error : Could not create socket");
 
     serv_addr.sin_family = AF_INET;
     serv_addr.sin_port = htons(MYPORT);
 
-    if(inet_pton(AF_INET, LOCALHOST, &serv_addr.sin_addr) <= 0) {
-        perror("\n inet_pton error occured\n");
-        return 1;
-    }
+    if(inet_pton(AF_INET, LOCALHOST, &serv_addr.sin_addr) <= 0)
+        err_n_die("inet_pton error occured");
 
-    if( connect(sockfd, (struct sockaddr *)&serv_addr, sizeof(serv_addr)) < 0) {
-        perror("\n Error : Connect Failed \n");
-        return 1;
-    }
+    if( connect(sockfd, (SA*)&serv_addr, sizeof(serv_addr)) < 0)
+        err_n_die("Error : Connect Failed");
 
-    resvArgs_t s_resv;
-    s_resv.connfd = sockfd;
-    s_resv.resvBuff = resvBuff;
+    //two struct for read/write buffer
+    funcArgc_t s_resv, s_send;
 
-    sendArgs_t s_send;
-    s_send.connfd = sockfd;
-    s_send.sendBuff = sendBuff;
+    s_resv.fd = sockfd;
+    s_resv.buff = resvBuff;
+    s_send.fd = sockfd;
+    s_send.buff = sendBuff;
 
-    pthread_t write_p, read_p;
-    if(pthread_create(&write_p, NULL, &Write, (void*) &s_send) != 0) {
-        perror("\n write_p create fail\n");
-        return 1;
-    }
-    if(pthread_create(&read_p, NULL, &Read, (void*) &s_resv) != 0) {
-        perror("\n read_p create fail\n");
-        return 1;
-    }
-    if(pthread_join(write_p, NULL) != 0) {
-        perror("\n write_p join fail\n");
-        return 1;
-    }
-    if(pthread_join(read_p, NULL) != 0) {
-        perror("\n read_p join fail\n");
-        return 1;
-    }
+    write(1, ">", 1);
+    //create & join two threads for Read() and Write() func
+    pthread_t read_p, write_p;
+   
+    create_thread(&read_p, &Read, &s_resv);
+    create_thread(&write_p, &Write, &s_send);
+
+    join_thread(&read_p);
+    join_thread(&write_p);
+
     return 0;
 }
 
+void create_thread(pthread_t *thread, void *p_func, funcArgc_t *s_struct) {
+    if(pthread_create(thread, NULL, p_func,(void *) s_struct) != 0)
+    err_n_die("thread create fail"); 
+}
+
+void join_thread(pthread_t *thread) {
+    if(pthread_join(*thread, NULL) != 0)
+    err_n_die("thread join fail");
+}
+
 void* Read(void *args) {
-    resvArgs_t *my_arg = (resvArgs_t*)args;
+    funcArgc_t *my_arg = (funcArgc_t*)args;
 
     int n;
-    while( (n = read(my_arg->connfd, my_arg->resvBuff, sizeof(my_arg->resvBuff) - 1)) > 0) {
-        my_arg->resvBuff[n] = 0;
-        write(1, "> ", 2);
+    while( (n = read(my_arg->fd, my_arg->buff, sizeof(my_arg->buff) - 1)) > 0) {
+        my_arg->buff[n] = 0;   
         fflush(stdout); 
-        
-        if(fputs(my_arg->resvBuff, stdout) == EOF) {
+    
+        if(fputs(my_arg->buff, stdout) == EOF) {
             perror("\n Error : Fputs error\n");
         }
         fflush(stdout); 
 
-        write(1, "\n\n", 2);
+        write(1, "\n\n>", 3);
         fflush(stdout); 
     }
     if(n < 0)
@@ -178,14 +146,35 @@ void* Read(void *args) {
 }
 
 void* Write(void *args) {
-    sendArgs_t *my_arg = (sendArgs_t*)args;
+    funcArgc_t *my_arg = (funcArgc_t*)args;
 
     while(1) {
-        write(1, ">", 1);
-        scanf("%1024s", my_arg->sendBuff);
+        scanf("%1024s", my_arg->buff);
         fpurge(stdin);
+        write(1, ">", 1);
 
-        write(my_arg->connfd, my_arg->sendBuff, strlen(my_arg->sendBuff));
+        write(my_arg->fd, my_arg->buff, strlen(my_arg->buff));
    }
 }
 
+void err_n_die(const char *format, ...){
+    int errno_save = errno;
+    va_list ap;
+
+    //print format+args to stdout
+    va_start(ap, format);
+    vfprintf(stdout, format, ap);
+    fprintf(stdout, "\n");
+    fflush(stdout);
+
+    //print error msg if errno != 0
+    if(errno_save != 0) {
+        fprintf(stdout, "(errno = %d) : %s\n", errno_save, strerror(errno_save));
+        fprintf(stdout, "\n");
+        fflush(stdout);
+    }
+    va_end(ap);
+
+    //die with error
+    exit(1);
+}   
--- clientServer.h
+++ clientServer.h
@@ -1,13 +1,8 @@
 #ifndef __CLIENTSERVER_H
 #define __CLIENTSERVER_H
 
-//#include <sys/socket.h>
-
-
-
-
-
-//d#include <netinet/in.h>
+#include <sys/socket.h>
+#include <sys/types.h>
 #include <netinet/in.h>
 #include <string.h>
 #include <arpa/inet.h>
@@ -15,30 +10,38 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <pthread.h>
-//#include <sys/types.h>
-
+#include <stdarg.h>
+#include <errno.h>
 
 //localhost
 #define LOCALHOST "127.0.0.1"
 #define MYPORT 8080
 
+//strings
 #define SERVER "Server"
 #define CLIENT "Client"
 
-typedef struct sendArgs_tag {
-    int connfd;
-    char *sendBuff;
-} sendArgs_t;
+//for beauty)
+#define SA struct sockaddr
 
-typedef struct resvArgs_tag {
-    int connfd;
-    char *resvBuff;
-} resvArgs_t;
+//struct for arguments in pthread_create
+typedef struct funcArgc_tag {
+    int fd;
+    char *buff;
+} funcArgc_t;
 
 int Server();
 int Client();
 
+//thread func
+void create_thread(pthread_t *thread, void *p_func, funcArgc_t *s_struct);
+void join_thread(pthread_t *thread);
+
+//read/write to socket
 void* Read(void *args);
 void* Write(void *args);
+
+//error print and ixit(1)
+void err_n_die(const char *format, ...);
 
 #endif
